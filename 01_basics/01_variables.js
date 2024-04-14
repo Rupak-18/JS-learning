d2a1687a84cff172beff1f6ca12672460e961d85
Rupak Das <161941702+Rupak-18@users.noreply.github.com>
2024-04-14 11:39:19 +0530

    # commit 2

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,11 @@
+const accountId = 12390
+let accountEmail = "rd@gmail.com"
+var accountCity = "kolkata"
+accountState = "WB"
+let city;
+
+// accountId = 34667 //not allowed for const
+// prefer not to use var for issues in block scope & functional scope
+
+console.table([accountId, accountEmail, accountCity, accountState, city])
+
